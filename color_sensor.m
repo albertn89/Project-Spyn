df53a6b50376e23f1d71f389b697c78b67938a90
Albert Negoro <56742226+albertn89@users.noreply.github.com>
2021-10-29 21:04:01 -0700

    Update color_sensor.m

--- color_sensor.m
+++ color_sensor.m
@@ -1,5 +1,5 @@
 %{
-ports:
+Port usage / description
 color: 2
 ultrasonic: 3
 color: 2
@@ -9,16 +9,7 @@ right motor: A
 crane motor: C
 %}
 
-
-
-% Set Color Sensor connected to Port 3 to Color Code Mode
-brick.SetColorMode(2, 2);
-  
-
-brick.MoveMotor('AD', 40 );
-while 1
-    %Get the color sensor reading from Port 3
-    col = brick.ColorCode(2);
+% Color code guide for the reading from the color sensor
 % 0 No color (Unknown color) 
 % 1 Black 
 % 2 Blue 
@@ -27,18 +18,34 @@ while 1
 % 5 Red 
 % 6 White 
 % 7 Brown
+
+% Set Color Sensor connected to Port 3 to Color Code Mode
+brick.SetColorMode(2, 2);
+  
+% Moving the vehicle forward
+brick.MoveMotor('AD', 40 );
+
+% While true (the motor is moving)
+while 1
+    % Get the color sensor reading from Port 3
+    col = brick.ColorCode(2);
+
+    % If color is Red (code 5)
     if col == 5
         brick.StopMotor('AD');        
         break
     
+    % If color is Yellow (code 4)
     elseif col == 4
         brick.StopMotor('AD');        
         break
     
+    % If color is Green (code 3)
     elseif col == 3
         brick.StopMotor('AD');        
         break
     
+    % If color is Blue (code 2)
     elseif col == 2
         brick.StopMotor('AD');        
         break
